2830c796c02b19e0fe5b542c86e3b30adc8131bb
egyudb <egyudb@gmail.com>
2019-02-23 16:24:23 +0100

    init2

---
+++ src/main/java/com/mycompany/heromarsspring/HeromarsspringApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.heromarsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeromarsspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeromarsspringApplication.class, args);
+	}
+
+}
