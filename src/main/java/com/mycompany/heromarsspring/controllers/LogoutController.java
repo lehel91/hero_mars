c5740cae7096e6fd6dea52ae2105ca9ad50968bc
malmos.peter <malmos.peter@gmail.com>
2019-03-07 23:36:22 +0100

    Logout is working
    Logout button must be designed later.

---
+++ src/main/java/com/mycompany/heromarsspring/controllers/LogoutController.java
@@ -0,0 +1,20 @@
+package com.mycompany.heromarsspring.controllers;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class LogoutController {
+
+	@RequestMapping(value = "logout", method = RequestMethod.GET)
+	public String logut(HttpSession session) {
+
+		session.invalidate();
+
+		return "index.html";
+	}
+}
