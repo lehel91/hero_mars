bd1fa496a35b665e4f29e2ac5b073a861c32ca27
malmos.peter <malmos.peter@gmail.com>
2019-03-09 15:47:09 +0100

    Modifying_skills

--- src/main/java/com/mycompany/heromarsspring/model/SkillEnum.java
+++ src/main/java/com/mycompany/heromarsspring/model/SkillEnum.java
@@ -14,29 +14,36 @@ import lombok.Getter;
 @Getter
 public enum SkillEnum {
     
-    WELLDRILLING_PADAVAN(1,1,1,0.2),
-    WELLDRILLING_MASTER(3,2,2,0.4),
-    WELLDRILLING_MAGE(6,3,3,0.6),
-    HUNTER_PADAVAN(1,2,1,0.2),
-    HUNTER_MASTER(2,4,2,0.4),
-    HUNTER_MAGE(4,6,3,0.6),
-    ASTRONOMER_PADAVAN(1,1,2,0.2),
-    ASTRONOMER_MASTER(2,2,4,0.4),
-    ASTRONOMER_MAGE(3,3,6,0.6),
-    TREASURE_HUNTER_PADAVAN(2,1,1,0.2),
-    TREASURE_HUNTER_MASTER(4,3,2,0.4),
-    TREASURE_HUNTER_MAGE(6,6,3,0.6);
+    WELLDRILLING_PADAVAN("Kútfúró inas",1,1,1,0.2,5),
+    WELLDRILLING_MASTER("Kútfúró mester",3,2,2,0.4,7),
+    WELLDRILLING_MAGE("Kútfúró mágus",6,3,3,0.6,9),
+    HUNTER_PADAVAN("Vadász inas",1,1,1,0.2,6),
+    HUNTER_MASTER("Vadász mester",2,4,2,0.4,8),
+    HUNTER_MAGE("Vadász mágus",4,6,3,0.6,10),
+    ASTRONOMER_PADAVAN("Csillagvizsgáló inas",1,1,1,0.2,7),
+    ASTRONOMER_MASTER("Csillagvizsgáló mester",2,2,4,0.4,9),
+    ASTRONOMER_MAGE("Csillagvizsgáló mágus", 3,3,6,0.6,11),
+    TREASURE_HUNTER_PADAVAN("Kincsvadász inas",1,1,1,0.2,8),
+    TREASURE_HUNTER_MASTER("Kincsvadász mester",4,3,2,0.4,10),
+    TREASURE_HUNTER_MAGE("Kincsvadász mágus",6,6,3,0.6,12);
     
+	private final String description;
     private final Integer skillHpMod;
     private final Integer skillStrengthMod;
     private final Integer skillWisdomMod;
     private final Double successRate;
+    private final Integer actionPointCost;
     
-	private SkillEnum(Integer skillHpMod, Integer skillStrengthMod, Integer skillWisdomMod, Double successRate) {
+	private SkillEnum(String description, Integer skillHpMod, Integer skillStrengthMod, Integer skillWisdomMod,
+			Double successRate, Integer actionPointCost) {
+		this.description = description;
 		this.skillHpMod = skillHpMod;
 		this.skillStrengthMod = skillStrengthMod;
 		this.skillWisdomMod = skillWisdomMod;
 		this.successRate = successRate;
+		this.actionPointCost = actionPointCost;
 	}
+    
+    
     
 }
