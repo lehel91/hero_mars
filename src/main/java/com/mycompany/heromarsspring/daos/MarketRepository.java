60ded48de11111096867eae6412cd32b54329e2c
malmos.peter <malmos.peter@gmail.com>
2019-03-13 23:46:01 +0100

    Marketplace 0.2

--- src/main/java/com/mycompany/heromarsspring/daos/MarketRepository.java
+++ src/main/java/com/mycompany/heromarsspring/daos/MarketRepository.java
@@ -3,6 +3,7 @@ package com.mycompany.heromarsspring.daos;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.mycompany.heromarsspring.entities.ItemMarket;
@@ -12,5 +13,12 @@ public interface MarketRepository extends JpaRepository<ItemMarket, Integer>{
 
 	List<ItemMarket> findAll();
 	
+	@Query("select i from ItemMarket i order by i.price")
+	List<ItemMarket> orderedListByPrice();
+	
+	@Query("select i from ItemMarket i  where i.item.type = ?1")
+	List<ItemMarket> findByType(String typeValue);
+	
+	void deleteById(Integer id);
 	
 }
